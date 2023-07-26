2ad56d113b4105a02d36a4e0a29d29a920db02fa
Jiyoon Kim <rlawldbs0911@naver.com>
2023-07-26 15:41:37 +0900

    프로젝트 생성

---
+++ OhTaeSik/OhTaeSik/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  OhTaeSik
+//
+//  Created by 지윤 on 2023/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var isLoading: Bool = true
+        
+        var body: some View {
+            ZStack {
+                VStack {
+                    Text("Hello, world")
+                }
+                .padding()
+                
+                if isLoading {
+                    LaunchScreenView().transition(.opacity).zIndex(1)
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+                    withAnimation { isLoading.toggle() }
+                })
+            }
+        }
+    }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OhTaeSik/OhTaeSik/OhTaeSikApp.swift
@@ -0,0 +1,17 @@
+//
+//  OhTaeSikApp.swift
+//  OhTaeSik
+//
+//  Created by 지윤 on 2023/07/26.
+//
+
+import SwiftUI
+
+@main
+struct OhTaeSikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
